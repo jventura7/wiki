5125e6d3ae6cf8f448bb27e263b15ce23212508f
jventura7 <jpventura925@gmail.com>
2023-06-25 18:32:37 -0400

    completed create new page

--- client/app/create/page.tsx
+++ client/app/create/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Create() {
-  const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
+  const [pageTitle, setPageTitle] = useState<string>('');
+  const [pageContent, setPageContent] = useState<string>('');
+  const { push } = useRouter();
 
   const handleSubmit = async () => {
     try {
@@ -13,10 +15,14 @@ export default function Create() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          title,
-          content,
+          pageTitle,
+          pageContent,
         }),
       });
+      console.log(response);
+      if (response.ok) {
+        push(`/wiki/${pageTitle}`);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -29,17 +35,17 @@ export default function Create() {
           Page Title:
         </label>
         <input
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setPageTitle(e.target.value)}
           className="border-b border-gray-600 transition duration-500 focus:border-b-orange-400 focus:outline-none"
           id="page-title"
           name="page-title"
           type="text"
-          value={title}
+          value={pageTitle}
         ></input>
       </div>
       <textarea
-        onChange={(e) => setContent(e.target.value)}
-        value={content}
+        onChange={(e) => setPageContent(e.target.value)}
+        value={pageContent}
         placeholder="Enter page..."
         className="h-96 w-full max-w-md rounded-lg border border-black p-2 focus:outline-none md:max-w-3xl"
       ></textarea>
---
+++ server/entries/Docker.md
@@ -0,0 +1,3 @@
+## Docker
+
+is a set of platform as a service (PaaS) products that use OS-level virtualization to deliver software in packages called containers.
